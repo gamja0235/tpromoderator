d1cc55a731f9beff1749641d5b7f5f766c455842
gamja0235 <152290240+gamja0235@users.noreply.github.com>
2023-11-29 23:09:46 +0900

    Update popup.js

--- popup.js
+++ popup.js
@@ -18,11 +18,15 @@ class Initial {
         this.#modeExplain = obj
         this.#loadState = "DONE"
         this.#modeExplainInit()
-        this.#radioInit()
+        //this.#radioInit()
     }
     
     getHashs() {
         return this.#hashs
+    }
+    
+    getModeExplain() {
+        return this.#modeExplain
     }
     
     getExplainString(mode = "manualReview") {
@@ -48,7 +52,7 @@ class Initial {
         this.#modeExplain = modeCopy
     }
     
-    #radioInit() {
+    /*#radioInit() {
         let radios = document.querySelectorAll('input[name="modes"]');
         let label = document.querySelector('label[id="modeExplanation"]');
         radios.forEach((rs)=>{
@@ -67,7 +71,7 @@ class Initial {
         let checked = document.querySelector('input[name="modes"]:checked');
         checked.removeAttribute('checked');
         checked.click();
-    }
+    }*/
 }
 
 // 강제 창 크기 조절
@@ -82,6 +86,31 @@ window.onresize = (_=>{
 
 // 라디오 등등 초기화 클래스-인스턴트
 let init = new Initial();
-
+let modes = {
+    'modeExplain': init.getModeExplain(),
+    '#loadState': init.getLoadState(),
+    'modeList': ["manualReview", "dobaeAutoRemove", "blockedWordsSetting", "ALLMODE"],
+    'radioInit': function() {
+        let radios = document.querySelectorAll('input[name="modes"]');
+        let label = document.querySelector('label[id="modeExplanation"]');
+        radios.forEach((rs)=>{
+            rs.addEventListener("change", (e) => {
+                const current = e.currentTarget;
+                if(current.checked) {
+                    let val = current.value;
+                    if (this.modeList.indexOf(val) > -1) {
+                        label.innerText = this.modeExplain[val].replace(/다\.\s/gu, '다.\n');
+                    } else {
+                        console.log('오류');
+                    }
+                }
+            });
+        });
+        let checked = document.querySelector('input[name="modes"]:checked');
+        checked.removeAttribute('checked');
+        checked.click();
+    }
+}
+modes.radioInit();
 delete initial;
 init = 'undefined';
